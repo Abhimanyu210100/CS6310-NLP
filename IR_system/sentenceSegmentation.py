fa969bd91dd4651bc70deef5c8940113168d0eb4
Abhimanyu210100 <32592640+Abhimanyu210100@users.noreply.github.com>
2021-05-30 19:51:06 +0530

    Update sentenceSegmentation.py
    Deleted Naive method

--- IR_system/sentenceSegmentation.py
+++ IR_system/sentenceSegmentation.py
@@ -10,30 +10,6 @@ from nltk import sent_tokenize
 
 class SentenceSegmentation():
 
-	def naive(self, text):
-		"""
-		Sentence Segmentation using a Naive Approach
-
-		Parameters
-		----------
-		arg1 : str
-			A string (a bunch of sentences)
-
-		Returns
-		-------
-		list
-			A list of strings where each string is a single sentence
-		"""
-
-		segmentedText = None
-
-		#Fill in code here
-
-		return segmentedText
-
-
-
-
 
 	def punkt(self, text):
 		"""
